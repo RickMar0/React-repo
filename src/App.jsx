a31552eb7cbf198afe8255084b1125b0909402ad
RickMar0 <165326554+RickMar0@users.noreply.github.com>
2025-06-02 01:03:00 +0200

    - implemented reusable dynamic elements in App.jsx - added TabButton.jsx as dynamic element

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+
+  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
   return (
     <div>
       <Header />
@@ -16,7 +26,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("Components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
+          </menu>
+          {selectedTopic}
+        </section>
       </main>
     </div>
   );
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,14 @@
+import { Children } from "react";
+
+export default function TabButton({children, onSelect}) {
+  function handleClick() {
+    console.log(`Hello World!`);
+  }
+  return (
+    <li>
+      <button onClick={ onSelect }>
+        {children}
+      </button>
+    </li>
+  )
+}
